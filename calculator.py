13f5a7bc625dded83c6f17e5699b885c5012b474
Hamza <120500474+hamza-bangash@users.noreply.github.com>
2024-01-30 21:05:13 +0000

    cs50p lecture 0
    during this lecture i learned
    1. variables its datatype for example str , int and float
    2. comments
    3. function  and their scopes and return method

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# x = input("Enter value of x: ")
+# y = input("Enter value of y: ")
+
+# z = int(x) + int(y)
+
+# x = int(input("Enter the value of x: "))
+# y = int(input("Enter the value of y: "))
+
+
+# print(f"sum of x and y is : {x+y}")
+
+# def hello():
+#     print("hello")
+
+# name = input("whats your name: ")
+# hello()
+# print(name)
+
+def hello(name):
+    print("hello", name)
+
+name = input("whats your name? ")
+hello(name)
---
+++ hello.py
@@ -0,0 +1,21 @@
+#Asking from user whats their name is using variable 
+name = input("what's your name? ") 
+
+#saying hello user
+#print("hello, "+ name )
+#print(name) 
+#print("hello ,", name)
+
+
+# end arguments is given by defult given by \n which means move next line so you can change this
+
+#print("hello ", end="") 
+#print(name)
+
+# sep arguments is used for space between two or more argument which set by defluts you can change any other string
+
+#print("hello",name,sep="???")
+
+#print("hello ,\"friend\"") # escape (\  back lash )is used for escape to escape while compiling
+
+print(f"hello, {name}")
